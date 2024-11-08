116033cc77681179473be422521a2fab46651a07
Aline da Silva Pereira <alinespereira@usp.br>
2024-11-07 21:40:07 -0300

    Lição 5

--- pratica/05-dolar.R
+++ pratica/05-dolar.R
@@ -2,8 +2,16 @@ library(xml2)
 
 # URL + parâmetros
 
-url <- ""
-params <- list()
+base_url <- "https://ptax.bcb.gov.br"
+
+url <- glue::glue("{base_url}/ptax_internet/consultaBoletim.do?method=consultarBoletim")
+
+params <- list(
+  RadOpcao = 1,
+  DATAINI = "08/10/2024",
+  DATAFIM = "07/11/2024",
+  ChkMoeda = 61
+)
 
 # Requisição
 res <- httr::POST(
@@ -13,5 +21,14 @@ res <- httr::POST(
 )
 
 # Resposta
-html <- read_html(res)
+html <- xml2::read_html(res)
 
+file_path <- html |>
+  xml2::xml_find_first("//a") |>
+  xml2::xml_attr("href") 
+
+read.csv(
+  text=glue::glue("{base_url}{file_path}") |>
+    httr::GET() |>
+    httr::content(as="text"),
+  sep = ";", dec = ",", header = FALSE)
